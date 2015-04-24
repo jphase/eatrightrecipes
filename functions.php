54871fecf153a1b4db99a97423aa6bc6064395ff
Jeff Hays <jhays@moguldom.com>
2015-04-24 18:13:20 -0400

    add publicize support recipe post type

--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@ function err_scripts_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'err_scripts_styles' );
+
+// Actions to happen on init
+function err_init_actions() {
+	// Add publicize support to the recipe post type
+	add_post_type_support( 'recipe', 'publicize' );
+}
+add_action( 'init', 'err_init_actions' );
